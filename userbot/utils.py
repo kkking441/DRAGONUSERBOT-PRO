0e5ab0634f3473c1a28acca851ee5791d42ca391
LEGEND-LX <prawashkumar14gmail.com>
2022-02-13 12:02:42 +0000

    auto-fixes

--- userbot/utils.py
+++ userbot/utils.py
@@ -17,7 +17,7 @@ from telethon import events
 from telethon.tl.functions.channels import GetParticipantRequest
 from telethon.tl.types import ChannelParticipantAdmin, ChannelParticipantCreator
 
-from userbot import CMD_LIST, LEGEND, LOAD_PLUG, LOGS, SUDO_LIST, Legend, bot
+from userbot import CMD_LIST, LOAD_PLUG, LOGS, SUDO_LIST, Legend, bot
 from userbot.Config import Config
 from userbot.helpers.exceptions import CancelProcess
 from var import Var
