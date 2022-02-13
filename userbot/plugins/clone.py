6a4ae62c5e71bc98af8dfa106f69be5ec980efb9
【★༒✞︎©༒ʟɛɢɛռɖ✘-թπο】ρɾαωαรɦ-Malviya <87700009+LEGEND-LX@users.noreply.github.com>
2022-02-13 03:25:43 -0800

    Update clone.py

--- userbot/plugins/clone.py
+++ userbot/plugins/clone.py
@@ -10,9 +10,9 @@ from userbot.utils import admin_cmd
 
 from . import *
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Lêɠêɳ̃dẞøy"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "LEGENDX~PRO"
 
-DEFAULTUSERBIO = str(BIO_MSG) if BIO_MSG else "I am User Of LegendBot @LegendBot_Pros"
+DEFAULTUSERBIO = str(BIO_MSG) if BIO_MSG else "I am User Of DragonBot @DragonPro_UserBot"
 LOGGER_ID = Config.LOGGER_ID
 LOGGER = True
 hui = legend_mention
