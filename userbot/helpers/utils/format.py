a86e9f3778e441d77b04ff266ccf38d6600d2842
【★༒✞︎©༒ʟɛɢɛռɖ✘-թπο】ρɾαωαรɦ-Malviya <87700009+LEGEND-LX@users.noreply.github.com>
2022-02-16 07:26:20 -0800

    Update format.py

--- userbot/helpers/utils/format.py
+++ userbot/helpers/utils/format.py
@@ -1,7 +1,7 @@
 import datetime
 
 from bs4 import BeautifulSoup
-from telethon import markdown
+from markdown import markdown
 from telethon.tl.tlobject import TLObject
 from telethon.tl.types import MessageEntityPre
 from telethon.utils import add_surrogate
